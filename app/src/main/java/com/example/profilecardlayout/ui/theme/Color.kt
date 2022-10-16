1c2e8edd19cfce15e5726ef27136755ea03c2d60
NawafIT <nwef918@gmail.com>
2022-10-17 00:17:28 +0300

    my project

---
+++ app/src/main/java/com/example/profilecardlayout/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.example.profilecardlayout.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+
+val veryLightGrey = Color(0xFFF1F1F1)
+val lightGreen200 = Color(0xD54CFF4C)
+
